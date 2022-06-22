ce52294a8fbce20727b8e4ffe0d02dd56b9364fd
Zkhogiani0 <Zkhogiani0@gmail.com>
2022-06-22 13:40:36 -0400

    second commit

--- src/Jenkins/Main.java
+++ src/Jenkins/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
             System.out.println("hello am i cat");
+            System.out.println("Hello! i am a human");
         }
     }
 }
